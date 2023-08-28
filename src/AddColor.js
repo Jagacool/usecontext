c40f1e3cd2f8daa369b3736dd754a0d74401509c
j <137029997+Jagacool@users.noreply.github.com>
2023-08-28 13:56:50 +0530

    Update AddColor.js

--- src/AddColor.js
+++ src/AddColor.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ColorBox } from "./ColorBox";
 
 export function AddColor() {
+  // const color = "skyblue";
   const [color, setColor] = useState("skyblue");
-  const [colorList, setColorList] = useState(["orange", "crimson", "pink"]);
 
+  // const colorList = ["orange", "crimson", "pink", "skyblue"];
+  const [colorList, setColorList] = useState(["orange", "crimson", "pink"]);
   const styles = {
     backgroundColor: color,
   };
-
   return (
     <div>
       <div className="add-color">
@@ -16,18 +17,19 @@ export function AddColor() {
           style={styles}
           type="text"
           onChange={(event) => setColor(event.target.value)}
-          value={color}
-        />
+          value={color} />
         <button
+          //copy the colorList and add new color to it
           onClick={() => {
             setColorList([...colorList, color]);
+            console.log(colorList, color);
           }}
         >
           Add color
         </button>
       </div>
-      {colorList.map((clr, index) => (
-        <ColorBox key={index} clr={clr} />
+      {colorList.map((clr) => (
+        <ColorBox clr={clr} />
       ))}
     </div>
   );

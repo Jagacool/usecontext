003b480ea827e31e2714b2d154b3a3ae5d540be2
j <137029997+Jagacool@users.noreply.github.com>
2023-08-28 12:13:26 +0530

    Create Welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+function Welcome({ name, age }) {
+  return (
+    //Fragment => React.Fragment
+    <>
+      <h1>
+        Hey {name} {age}
+      </h1>
+      {/* <h1>Hey {props.name} </h1> */}
+    </>
+  );
+}

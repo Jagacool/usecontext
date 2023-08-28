aa68dc024169cc2a24ded20eea3bcd5d99a944b2
j <137029997+Jagacool@users.noreply.github.com>
2023-08-28 12:07:36 +0530

    Create Counter.js

---
+++ src/Counter.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+export function Counter() {
+  // let like = 10;
+  const [like, setLike] = useState(0);
+  const [dislike, setDisLike] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("Total updates", like + dislike);
+  // }, [like, dislike]); //called everytime
+
+  return (
+    <>
+      {/* onClick => camelCase */}
+      <IconButton
+        aria-label="like-btn"
+        onClick={() => {
+          setLike(like + 1);
+          // console.log(like);
+        }}
+        color="primary"
+      >
+        <Badge badgeContent={like} color="primary">
+          👍
+        </Badge>
+      </IconButton>
+
+      <IconButton
+        aria-label="dislike-btn"
+        onClick={() => {
+          setDisLike(dislike + 1);
+          // console.log(dislike);
+        }}
+        color="error"
+      >
+        <Badge badgeContent={dislike} color="error">
+          👎
+        </Badge>
+      </IconButton>
+    </>
+  );
+}

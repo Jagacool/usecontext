c9dd0bdebe9aefe233c083438a919266e34c804a
j <137029997+Jagacool@users.noreply.github.com>
2023-08-28 12:45:27 +0530

    Update and rename src/components/AddColour.js to src/AddColour.js

--- src/AddColour.js
+++ src/AddColour.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ColorBox } from "./ColorBox";
 
 export function AddColor() {
-  // const color = "skyblue";
   const [color, setColor] = useState("skyblue");
-
-  // const colorList = ["orange", "crimson", "pink", "skyblue"];
   const [colorList, setColorList] = useState(["orange", "crimson", "pink"]);
+
   const styles = {
     backgroundColor: color,
   };
+
   return (
     <div>
       <div className="add-color">
@@ -17,19 +16,18 @@ export function AddColor() {
           style={styles}
           type="text"
           onChange={(event) => setColor(event.target.value)}
-          value={color} />
+          value={color}
+        />
         <button
-          //copy the colorList and add new color to it
           onClick={() => {
             setColorList([...colorList, color]);
-            console.log(colorList, color);
           }}
         >
           Add color
         </button>
       </div>
-      {colorList.map((clr) => (
-        <ColorBox clr={clr} />
+      {colorList.map((clr, index) => (
+        <ColorBox key={index} clr={clr} />
       ))}
     </div>
   );
